b161097c937a8e9c858d7a94b0bb93921f6132bf
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-21 10:27:12 +0530

    Usercse3:checking for proper email

--- userregistation.sh
+++ userregistation.sh
@@ -2,9 +2,16 @@
 
 	read -p "Enter the name of user " user
 
-	name="^[A-Z]{1}[a-z]{3,}$"
+	firstname="^[A-Z]{1}[a-z]{3,}$"
 	lastname="^[A-Z]{1}[a-z]{3,}$"
-	if [[ $user =~ $lastname ]]
+	name="^[a-zA-Z]{1,}[0-9]*"
+	optionalname="[._+-]{1,}[a-zA-Z0-9]+"
+	emailname="[@][a-zA-Z0-9]+"
+	emaillast="[.][a-z]{2,4}"
+	optionallastname="[.][a-z]{2}"
+	email="^[a-zA-Z]{1,}[0-9]*([._+-]{1}[a-zA-Z0-9]+){0,1}[@][a-zA-Z0-9]+[.][a-z]{2,4}([.][a-z]{2}){0,1}$"
+
+	if [[ $user =~ $email ]]
 	then
 		echo "Valid"
 	else
