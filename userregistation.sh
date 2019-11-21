51e5a330d4f98559c2ad6d775b826e9091b0bac8
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-21 10:43:28 +0530

    Refactored code as follows

--- userregistation.sh
+++ userregistation.sh
@@ -1,6 +1,7 @@
 #regex pattern
 
 	read -p "Enter the name of user " user
+	read -p "Enter the mobilenumber of user " mobilenum
 
 	firstname="^[A-Z]{1}[a-z]{3,}$"
 	lastname="^[A-Z]{1}[a-z]{3,}$"
@@ -10,8 +11,15 @@
 	emaillast="[.][a-z]{2,4}"
 	optionallastname="[.][a-z]{2}"
 	email="^[a-zA-Z]{1,}[0-9]*([._+-]{1}[a-zA-Z0-9]+){0,1}[@][a-zA-Z0-9]+[.][a-z]{2,4}([.][a-z]{2}){0,1}$"
-
-	if [[ $user =~ $email ]]
+	mobilenumber="^[9][1][[:space:]][6-9]{1}[0-9]{9}$"
+	fullname="^[A-Z]{1}[a-z]{3,}[A-Z]{1}[a-z]{3,}$"
+	if [[ $user =~ $fullname ]]
+	then
+		echo "valid"
+	else
+		echo "not valid"
+        fi   
+	if [[ $mobilenum =~ $mobilenumber ]]
 	then
 		echo "Valid"
 	else
